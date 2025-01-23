52ec1b9313ba2337603d101c152bbfb75edcbd36
Henri Aïdasso <ahenrij@gmail.com>
2025-01-23 02:00:16 -0500

    feat: labeler impl.

--- src/flakeranker/labeler/label.py
+++ src/flakeranker/labeler/label.py
@@ -1,23 +1,73 @@
 import re
+import os
+import sys
+import itertools
+import pandas as pd
+from tqdm import tqdm
+
+from src.flakeranker import utils
 
 
-def infer_category(row, patterns):
-    """Infer job category and subcategory based on manual patterns."""
-    if row["category"] is not None:
-        return row["category"], row["subcategory"]
-    if row["failure_reason"] == "stuck_or_timeout_failure":
-        return "limits_exceeded", row["failure_reason"]
+tqdm.pandas()
+dirname = os.path.dirname(__file__)
+
+
+def label_job(job, patterns):
+    """Infer job category based on manual patterns."""
+    if job["category"] is not None:
+        return job["category"]
+    if job["failure_reason"] == "stuck_or_timeout_failure":
+        return job["failure_reason"]
     for _, pattern in patterns.iterrows():
-        if re.search(pattern["regex"], row["log"], flags=re.IGNORECASE) is not None:
-            return pattern["category"], pattern["subcategory"]
-    return None, None
+        try:
+            if (
+                re.search(pattern["regex"], job["logs"], flags=re.IGNORECASE)
+                is not None
+            ):
+                return pattern["category"]
+        except MemoryError:
+            continue
+    return None
 
 
-def infer(df, output: str):
-    """Automatically assing jobs to categories and subcategories based on manual regex patterns"""
-    df["category"], df["subcategory"] = zip(*df.apply(infer_category, axis=1))
-    df.to_csv(output, index=False)
+def label(input_file_path: str, output_file_path: str):
+    """Automatically label (add label columns described below to) a dataset of build jobs.
 
-    # Check the obtained coverage
-    n_clustered = df[~df["category"].isna()].shape[0]
-    print(f"{round(float(n_clustered / df.shape[0]) * 100, 2)}% of jobs are assigned")
+    Returns
+    -------
+        The input `.csv` dataset with the following additional columns.
+        `flaky` (bool): Indicating whether the job is flaky or not.
+        `category` (str): Category of flaky job failure, available only for flaky failed jobs successfully identified by the regexes.
+    """
+    df = pd.read_csv(input_file_path)
+
+    # label flakiness
+    flaky_reruns = utils.list_flaky_rerun_suites(df)
+    flaky_job_ids = list(itertools.chain(*flaky_reruns["id"].to_list()))
+    df["flaky"] = df.progress_apply(lambda job: job["id"] in flaky_job_ids)
+
+    # label on flaky job failure with categories
+    patterns = pd.read_csv(os.path.join(dirname, "patterns.csv"))
+    flaky_job_failures = df[df["flaky"] & df["status"] == "failed"]
+    flaky_job_failures["category"] = flaky_job_failures.progress_apply(
+        lambda row: label_job(row, patterns), axis=1
+    )
+
+    # Join labeled job to the full dataset
+    df = utils.join_dfs(df, flaky_job_failures[["id", "category"]], key="id")
+    labeled_df = df[~df["category"].isna()]
+
+    # Export
+    df.to_csv(output_file_path, index=False)
+
+    # Display the obtained coverage
+    n_labeled = labeled_df.shape[0]
+    recall = round(float(n_labeled / df.shape[0]) * 100, 2)
+    print(f"{recall}% of jobs are labeled with a category.")
+    return recall
+
+
+if __name__ == "__main__":
+    input = sys.argv[1]
+    output = sys.argv[2]
+    label(input, output)

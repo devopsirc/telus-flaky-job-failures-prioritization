5a3b3e96f48893821ddf1c44ff3e463a7d8bf755
Henri Aïdasso <ahenrij@gmail.com>
2025-01-24 00:53:58 -0500

    fix: rank impl.

--- src/flakeranker/cli/main.py
+++ src/flakeranker/cli/main.py
@@ -3,7 +3,7 @@
 import os
 import click
 
-from src.flakeranker import labeler, analyzer
+from src.flakeranker import labeler, analyzer, ranker
 
 
 @click.group(invoke_without_command=False)
@@ -63,21 +63,32 @@ def label(input_file: str, output_dir: str):
     required=True,
 )
 def analyze(input_file: str, output_dir: str):
-    """Analyze an INPUT_FILE labeled dataset of build jobs, output from the label sub-command."""
+    """Analyze an INPUT_FILE labeled dataset of build jobs, the output from the label sub-command.
+    
+    Outputs the RFM dataset of the categories.
+    """
     click.echo("Analyzing...")
-    output_file_path = os.path.join(output_dir, "rfm.csv")
+    output_file_path = os.path.join(output_dir, "rfm_dataset.csv")
     analyzer.analyze(input_file, output_file_path)
+    click.echo(click.style("Analysis successfully finished! ", fg="green"))
 
 
-@cli.command(help="Rank flaky job failure categories with RFM patterns.")
+@cli.command()
+@click.argument(
+    "input_file", type=click.Path(exists=True, dir_okay=False, readable=True)
+)
 @click.option(
     "-o",
     "--output-dir",
     type=click.Path(exists=True, file_okay=False, writable=True),
     required=True,
 )
-def rank():
+def rank(input_file: str, output_dir: str):
+    """Rank flaky job failure categories using an INPUT_FILE rfm dataset, the output of the analyze sub-command"""
     click.echo("Ranking...")
+    output_file_path = os.path.join(output_dir, "ranked_rfm_dataset.csv")
+    ranker.rank(input_file, output_file_path)
+    click.echo(click.style("Ranking successfully finished! ", fg="green"))
 
 
 if __name__ == "__main__":
--- src/flakeranker/ranker/rank.py
+++ src/flakeranker/ranker/rank.py
@@ -1,6 +1,7 @@
 """Module for ranking flaky job failure categories based on RFM measures, i.e. Output of Analyzer."""
 
 import sys
+import click
 import numpy as np
 import pandas as pd
 from tqdm import trange
@@ -18,7 +19,6 @@ def find_outliers(rfm: pd.DataFrame):
 
 def preprocess(rfm: pd.DataFrame) -> pd.DataFrame:
     outliers = find_outliers(rfm)
-    outliers.to_csv("outliers.csv", index=False)
 
     # remove outliers
     rfm = rfm[~rfm["category"].isin(outliers["category"])]
@@ -27,38 +27,118 @@ def preprocess(rfm: pd.DataFrame) -> pd.DataFrame:
     rfm["R"] = pd.qcut(rfm["recency"], q=5, labels=list(range(5, 0, -1)))
     rfm["F"] = pd.qcut(rfm["frequency"], q=5, labels=list(range(1, 6)))
     rfm["M"] = pd.qcut(rfm["cost"], q=5, labels=list(range(1, 6)))
+    return outliers, rfm
 
-    return rfm
+
+def create_model(rfm_scores: pd.DataFrame):
+    inertias = []
+    models = []
+    for _ in trange(500, desc="Fitting clustering models"):
+        kmeans = KMeans(n_clusters=8, init="k-means++")
+        kmeans.fit(rfm_scores)
+        models.append(kmeans)
+        inertias.append(kmeans.inertia_)
+
+    i = np.argmin(inertias)
+    click.echo(f"Lowest clustering inertia: {min(inertias)}")
+    return models[i]
+
+
+def cluster_rfm_pattern(row, cluster_stats):
+    r_mean = cluster_stats["R"].mean()
+    f_mean = cluster_stats["F"].mean()
+    m_mean = cluster_stats["M"].mean()
+
+    pattern = ""
+    pattern += "R+" if row["R"] > r_mean else "R-"
+    pattern += "F+" if row["F"] > f_mean else "F-"
+    pattern += "M+" if row["M"] > m_mean else "M-"
+    return pattern
+
+
+def outlier_rfm_pattern(row, rfm_dataset):
+    r_mean = rfm_dataset["recency"].mean()
+    f_mean = rfm_dataset["frequency"].mean()
+    m_mean = rfm_dataset["cost"].mean()
+
+    pattern = ""
+    pattern += "R+" if row["recency"] < r_mean else "R-"
+    pattern += "F+" if row["frequency"] > f_mean else "F-"
+    pattern += "M+" if row["cost"] > m_mean else "M-"
+    return pattern
+
+
+def compute_cluster_statistics(results: pd.DataFrame):
+    cluster_stats = (
+        results.drop(columns=["category"])
+        .groupby("cluster")
+        .agg(["count", "mean"])
+        .reset_index()
+        .round(2)
+    )
+    cluster_stats.columns = [
+        "cluster",
+        "#",
+        "R",
+        "a",
+        "F",
+        "a",
+        "M",
+        "a",
+        "recency",
+        "a",
+        "frequency",
+        "a",
+        "cost",
+    ]
+    cluster_stats = cluster_stats[
+        ["cluster", "#", "R", "F", "M", "recency", "frequency", "cost"]
+    ]
+    cluster_stats["pattern"] = cluster_stats.apply(
+        lambda row: cluster_rfm_pattern(row, cluster_stats), axis=1
+    )
+    return cluster_stats
+
+
+def process_outliers(outliers: pd.DataFrame, rfm: pd.DataFrame):
+    outliers["R"] = -1
+    outliers["F"] = -1
+    outliers["M"] = -1
+    outliers["cluster"] = -1
+    outliers["pattern"] = outliers.apply(
+        lambda row: outlier_rfm_pattern(row, rfm), axis=1
+    )
+    return outliers
 
 
 def rank(input_file_path: str, output_file_path: str):
     # Read input data
     rfm = pd.read_csv(input_file_path)[["category", "recency", "frequency", "cost"]]
-    rfm_scores = preprocess(rfm)
+    outliers, rfm_scores = preprocess(rfm)
+    outliers = process_outliers(outliers, rfm)
 
+    # Run clustering and get the model
     y = rfm_scores["category"]
-    X = rfm_scores.drop(columns=["category"]).astype(int)
+    X = rfm_scores[["R", "F", "M"]].astype(int)
+    model = create_model(X)
 
-    inertias = []
-    models = []
-    for _ in trange(500, desc="Fitting clustering models"):
-        kmeans = KMeans(n_clusters=8, init="k-means++")
-        kmeans.fit(X)
-        models.append(kmeans)
-        inertias.append(kmeans.inertia_)
+    # create results dataset
+    results = pd.DataFrame(X).astype(int)
+    results["cluster"] = model.labels_
+    results["category"] = y
+    results["recency"] = rfm["recency"]
+    results["frequency"] = rfm["frequency"]
+    results["cost"] = rfm["cost"]
+    results = results[
+        ["category", "R", "F", "M", "recency", "frequency", "cost", "cluster"]
+    ]
 
-    i = np.argmin(inertias)
-    print(f"Lowest clustering inertia: {min(inertias)}")
+    cluster_stats = compute_cluster_statistics(results)[["cluster", "pattern"]]
+    results = results.join(cluster_stats.set_index("cluster"), on="cluster", validate="m:1")
 
-    model = models[i]
-
-    df_cluster = pd.DataFrame(X).astype(int)
-    df_cluster["cluster"] = model.labels_
-    df_cluster["category"] = y
-    df_cluster["Recency"] = rfm["Recency"]
-    df_cluster["Frequency"] = rfm["Frequency"]
-    df_cluster["Monetary"] = rfm["Monetary"]
-    df_cluster.head()
+    results = pd.concat([results, outliers], sort=False)
+    results.sort_values(by=["pattern", "R", "F", "M"], inplace=True)
+    results.to_csv(output_file_path, index=False)
 
 
 if __name__ == "__main__":
